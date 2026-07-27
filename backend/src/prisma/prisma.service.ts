c90f958db1e91d3c27c4c4800b5215164a783e9e
Delano Manafe <legacyditt11@gmail.com>
2026-07-27 22:55:55 +0800

    fix(backend): configure PrismaClient with pg adapter

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,15 +1,23 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
-@Injectable() // <-- Menjadikannya bisa di-inject (Dependency Injection)
+@Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+    
+    super({ adapter });
+  }
+
   async onModuleInit() {
-    // Dipanggil saat aplikasi NestJS mulai berjalan
     await this.$connect();
   }
 
   async onModuleDestroy() {
-    // Dipanggil saat aplikasi NestJS dimatikan
     await this.$disconnect();
   }
 }
