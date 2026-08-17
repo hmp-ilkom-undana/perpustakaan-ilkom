6a733cbf96f9bf51d8cbcb476283af8f27ba3a58
Delano Manafe <legacyditt11@gmail.com>
2026-08-17 20:57:07 +0800

    feat(business-logic): integrate dynamic settings for borrowing quotas, fines, and auto-cancel

--- backend/src/cron/cron.module.ts
+++ backend/src/cron/cron.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CronService } from './cron.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { SettingModule } from '../setting/setting.module';
 
 @Module({
+  imports: [PrismaModule, SettingModule],
   providers: [CronService],
 })
 export class CronModule {}
